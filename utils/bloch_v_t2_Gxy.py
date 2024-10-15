5239accd74e5f929db2be745dafeb6895b571661
Xingxin He <47737609+lphxx6222712@users.noreply.github.com>
2024-10-15 14:13:38 -0400

    Update bloch_v_t2_Gxy.py

--- utils/bloch_v_t2_Gxy.py
+++ utils/bloch_v_t2_Gxy.py
@@ -16,7 +16,7 @@ class Gxyt2fBlochsim_fast_B0_jit(jit.ScriptModule): #online learing faster 2D si
     @jit.script_method
     def forward(self, rRF, iRF, Gx, Gy, B_0, B_1):
         sRF_dt = self.dt
-        sM_x = torch.linspace(-self.FOV/2, self.FOV/2, self.nvox).cuda() #[128]
+        sM_x = torch.linspace(-self.FOV/2, self.FOV/2, self.nvox).cuda()
         sM_y = torch.linspace(-self.FOV/2, self.FOV/2, self.nvoy).cuda()
 
         mx = torch.zeros(rRF.size()[0], self.nvox, self.nvoy, len(self.t2)).cuda()
@@ -24,21 +24,16 @@ class Gxyt2fBlochsim_fast_B0_jit(jit.ScriptModule): #online learing faster 2D si
         mz = torch.ones(rRF.size()[0], self.nvox, self.nvoy,  len(self.t2)).cuda()
 
         for i in range(rRF.size(-1)):
-            m = torch.cat((mx.unsqueeze(-1), my.unsqueeze(-1), mz.unsqueeze(-1)), -1) #[B,nvox,nvoy,t2,3]
-            m = m.reshape([m.size(0), m.size(1)*m.size(2)*m.size(-1),1])#[B,nvox*nvoy*3,1]
+            m = torch.cat((mx.unsqueeze(-1), my.unsqueeze(-1), mz.unsqueeze(-1)), -1)
+            m = m.reshape([m.size(0), m.size(1)*m.size(2)*m.size(-1),1])
 
             sM_f = (B_0 + self.GAMMA * (torch.einsum('b, c->bc', Gx[:, i], sM_y).unsqueeze(1) +
                                  torch.einsum('b, c->bc', Gy[:, i], sM_x).unsqueeze(-1))).reshape(sM_x.size(0)*sM_y.size(0)).unsqueeze(0)
-
-
             b1 = torch.einsum('b,bs->bs', torch.sqrt(rRF[:,i]**2+iRF[:,i]**2), B_1.reshape([rRF.size(0), self.nvox*self.nvoy]))
-
             beff = torch.sqrt(b1 ** 2 + sM_f ** 2)
-
-            phi = torch.einsum('b,bs->bs', torch.atan2(iRF[:,i], rRF[:,i]), torch.ones(rRF.size(0), self.nvox*self.nvoy).cuda()) # angle between RF and Z
-
-            beta = torch.atan2(sM_f, b1) # angle between M and G
-            theta = 2 * torch.pi * beff * sRF_dt # rotation angle
+            phi = torch.einsum('b,bs->bs', torch.atan2(iRF[:,i], rRF[:,i]), torch.ones(rRF.size(0), self.nvox*self.nvoy).cuda())
+            beta = torch.atan2(sM_f, b1)
+            theta = 2 * torch.pi * beff * sRF_dt
 
             col1 = torch.zeros(rRF.size()[0], 3, self.nvox*self.nvoy).cuda()
             col2 = torch.zeros(rRF.size()[0], 3, self.nvox*self.nvoy).cuda()
@@ -64,23 +59,17 @@ class Gxyt2fBlochsim_fast_B0_jit(jit.ScriptModule): #online learing faster 2D si
             colinterleaved = torch.flatten(colstacked, start_dim=2, end_dim=3)
             aa = colinterleaved.transpose(1, 2).unsqueeze_(-1)
             bb = aa.reshape(rRF.size()[0], int(colinterleaved.size()[-1] / 3), 3, 3)
-            R_ = bb.transpose(2, 3).permute(0, 2, 3, 1)  # [b,3,3,nvox*nvoy]
-            R = torch.diag_embed(R_, 0, 1, 2)  # [b,nvox*nvoy,nvox*nvoy,3,3]
+            R_ = bb.transpose(2, 3).permute(0, 2, 3, 1)
+            R = torch.diag_embed(R_, 0, 1, 2)
 
             R = R.reshape([rRF.size()[0], self.nvox*self.nvoy, self.nvox*self.nvoy * 3, 3]).permute(0, 1, 3, 2).reshape(
-                [rRF.size()[0], self.nvox*self.nvoy * 3, self.nvox*self.nvoy * 3]).transpose(2, 1) #[b,nvox*nvoy * 3,nvox*nvoy * 3]
-
-            #m = torch.matmul(R, m)#.cuda()#[B,3nvox*nvoy,3nvox*nvoy] X [B,3nvox,1]
+                [rRF.size()[0], self.nvox*self.nvoy * 3, self.nvox*self.nvoy * 3]).transpose(2, 1)
             m = torch.sparse.mm(R.squeeze(0).to_sparse(), m.squeeze(0))
             m = m.unsqueeze(0)
 
             mx = m[:, 0:m.size()[1]:3, :].reshape(m.size(0), self.nvox, self.nvoy, 1)
             my = m[:, 1:m.size()[1]:3, :].reshape(m.size(0), self.nvox, self.nvoy, 1)
             mz = m[:, 2:m.size()[1]:3, :].reshape(m.size(0), self.nvox, self.nvoy, 1)
-
-            # if len(self.t2)>1:
-            #     mx = mx * torch.exp(-self.dt / self.t2).cuda()
-            #     my = my * torch.exp(-self.dt / self.t2).cuda()
 
         m = torch.cat([mx, my, mz], -1)
 
@@ -101,7 +90,7 @@ class Gxyt2fBlochsim_fast_jit(jit.ScriptModule):
     @jit.script_method
     def forward(self, rRF, iRF, Gx, Gy):
         sRF_dt = self.dt
-        sM_x = torch.linspace(-self.FOV/2, self.FOV/2, self.nvox).cuda() #[128]
+        sM_x = torch.linspace(-self.FOV/2, self.FOV/2, self.nvox).cuda()
         sM_y = torch.linspace(-self.FOV/2, self.FOV/2, self.nvoy).cuda()
         mx = torch.zeros(rRF.size()[0], self.nvox, self.nvoy, len(self.t2)).cuda()
         my = torch.zeros(rRF.size()[0], self.nvox, self.nvoy, len(self.t2)).cuda()
@@ -109,18 +98,17 @@ class Gxyt2fBlochsim_fast_jit(jit.ScriptModule):
         #sRF_b1 = rRF + iRF * 1j
 
         for i in range(rRF.size(-1)):
-            m = torch.cat((mx.unsqueeze(-1), my.unsqueeze(-1), mz.unsqueeze(-1)), -1) #[B,nvox,nvoy,t2,3]
-            m = m.reshape([m.size(0), m.size(1)*m.size(2)*m.size(-1),1])#[B,nvox*nvoy*3,1]
+            m = torch.cat((mx.unsqueeze(-1), my.unsqueeze(-1), mz.unsqueeze(-1)), -1)
+            m = m.reshape([m.size(0), m.size(1)*m.size(2)*m.size(-1),1])
 
             sM_f = self.GAMMA * (torch.einsum('b, c->bc', Gx[:, i], sM_y).unsqueeze(1) +
                                  torch.einsum('b, c->bc', Gy[:, i], sM_x).unsqueeze(-1)).reshape(sM_x.size(0)*sM_y.size(0)).unsqueeze(0)
 
             b1 = torch.einsum('b,bs->bs', torch.sqrt(rRF[:,i]**2+iRF[:,i]**2),
-                              torch.ones(rRF.size(0), self.nvox*self.nvoy).cuda()) #[b, 1, 256]
+                              torch.ones(rRF.size(0), self.nvox*self.nvoy).cuda())
             beff = torch.sqrt(b1 ** 2 + sM_f ** 2)
 
             phi = torch.einsum('b,bs->bs', torch.atan2(iRF[:,i],rRF[:,i]), torch.ones(rRF.size(0), self.nvox*self.nvoy).cuda()) # angle between RF and Z
-
             beta = torch.atan2(sM_f, b1) # angle between M and G
             theta = 2 * torch.pi * beff * sRF_dt # rotation angle
             col1 = torch.zeros(rRF.size()[0], 3, self.nvox*self.nvoy).cuda()
@@ -147,23 +135,17 @@ class Gxyt2fBlochsim_fast_jit(jit.ScriptModule):
             colinterleaved = torch.flatten(colstacked, start_dim=2, end_dim=3)
             aa = colinterleaved.transpose(1, 2).unsqueeze_(-1)
             bb = aa.reshape(rRF.size()[0], int(colinterleaved.size()[-1] / 3), 3, 3)
-            R_ = bb.transpose(2, 3).permute(0, 2, 3, 1)  # [b,3,3,nvox*nvoy]
-            R = torch.diag_embed(R_, 0, 1, 2)  # [b,nvox*nvoy,nvox*nvoy,3,3]
+            R_ = bb.transpose(2, 3).permute(0, 2, 3, 1)
+            R = torch.diag_embed(R_, 0, 1, 2)
 
             R = R.reshape([rRF.size()[0], self.nvox*self.nvoy, self.nvox*self.nvoy * 3, 3]).permute(0, 1, 3, 2).reshape(
-                [rRF.size()[0], self.nvox*self.nvoy * 3, self.nvox*self.nvoy * 3]).transpose(2, 1) #[b,nvox*nvoy * 3,nvox*nvoy * 3]
-
-            #m = torch.matmul(R, m)#.cuda()#[B,3nvox*nvoy,3nvox*nvoy] X [B,3nvox,1]
+                [rRF.size()[0], self.nvox*self.nvoy * 3, self.nvox*self.nvoy * 3]).transpose(2, 1)
             m = torch.sparse.mm(R.squeeze(0).to_sparse(), m.squeeze(0))
             m = m.unsqueeze(0)
 
             mx = m[:, 0:m.size()[1]:3, :].reshape(m.size(0), self.nvox, self.nvoy, 1)
             my = m[:, 1:m.size()[1]:3, :].reshape(m.size(0), self.nvox, self.nvoy, 1)
             mz = m[:, 2:m.size()[1]:3, :].reshape(m.size(0), self.nvox, self.nvoy, 1)
-
-            # if len(self.t2)>1:
-            #     mx = mx * torch.exp(-self.dt / self.t2).cuda()
-            #     my = my * torch.exp(-self.dt / self.t2).cuda()
 
         m = torch.cat([mx, my, mz], -1)
 
