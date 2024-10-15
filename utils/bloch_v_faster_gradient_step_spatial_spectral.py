795a580a3fcad9d5ae45bfe6f9e0883d971ddde9
Xingxin He <47737609+lphxx6222712@users.noreply.github.com>
2024-10-15 14:10:18 -0400

    Update bloch_v_faster_gradient_step_spatial_spectral.py

--- utils/bloch_v_faster_gradient_step_spatial_spectral.py
+++ utils/bloch_v_faster_gradient_step_spatial_spectral.py
@@ -20,16 +20,10 @@ class SPSPfBlochsim_fast_batch_faster(jit.ScriptModule):
         mx = torch.zeros(rRF.size()[0], self.nvox, len(t2)).cuda()
         my = torch.zeros(rRF.size()[0], self.nvox, len(t2)).cuda()
         mz = torch.ones(rRF.size()[0], self.nvox, len(t2)).cuda()
-
-
         m = torch.cat([mx, my, mz], -1)
-        # time_wise_profile = torch.zeros([mx.size(0), self.nvox * 3, sRF_b1.size(1)])
 
-        for i in range(rRF.size(1)):  # self.duration
-            #print(i)
-
-            # [B, 256, 3]
-            m = torch.reshape(m, (rRF.size()[0], m.size()[1] * m.size()[2], 1))  # [B, 3*NVOX, 1]
+        for i in range(rRF.size(1)):
+            m = torch.reshape(m, (rRF.size()[0], m.size()[1] * m.size()[2], 1))
 
             sM_f = (self.GAMMA*torch.einsum('b, bc->bc', Gx[:, i], sM_x) + S).unsqueeze(1)
 
@@ -40,7 +34,7 @@ class SPSPfBlochsim_fast_batch_faster(jit.ScriptModule):
             phi = torch.einsum('b,bsc->bsc', torch.atan2(iRF[:,i],rRF[:,i]),
                                torch.ones(rRF.size()[0], 1, self.nvox).cuda())
 
-            beta = torch.atan2(sM_f, b1)  # the angle between RF and
+            beta = torch.atan2(sM_f, b1)
             theta = 2 * torch.pi * beff * self.dt
 
             col1 = torch.zeros(rRF.size()[0], 3, beta.size()[-1]).cuda()
@@ -87,33 +81,15 @@ class SPSPfBlochsim_fast_batch_faster(jit.ScriptModule):
             colinterleaved = torch.flatten(colstacked, start_dim=2, end_dim=3)
             aa = colinterleaved.transpose(1, 2).unsqueeze_(-1)
             bb = aa.reshape(rRF.size()[0], int(colinterleaved.size()[-1] / 3), 3, 3)
-            R_ = bb.transpose(2, 3).permute(0, 2, 3, 1)  # [b,3,3,256]
+            R_ = bb.transpose(2, 3).permute(0, 2, 3, 1)
 
-            R = torch.diag_embed(R_, 0, 1, 2)  # [b,256,256,3,3]
+            R = torch.diag_embed(R_, 0, 1, 2)
             R = R.reshape([rRF.size()[0], self.nvox, self.nvox * 3, 3]).permute(0, 1, 3, 2).reshape(
-                [rRF.size()[0], self.nvox * 3, self.nvox * 3]).transpose(2, 1)  # [b,768,768]
-
-            #m = torch.matmul(R, m)#.cuda()#[B,3nvox,3nvox] X [B,3nvox,1]
+                [rRF.size()[0], self.nvox * 3, self.nvox * 3]).transpose(2, 1)
             m = torch.bmm(R.to_sparse(), m)
-            #
-            # m = torch.sparse.mm(R.squeeze(0).to_sparse(), m.squeeze(0))
-            # m = m.unsqueeze(0)
-
-            # mx = m[:, 0:m.size()[1]:3]
-            # my = m[:, 1:m.size()[1]:3]
-            # mz = m[:, 2:m.size()[1]:3]
-
-            # out = torch.cat([mx, my, mz], 1).squeeze(1).squeeze(2)
-            #
-            # res = {'Bloch_excitation': out.squeeze().detach().cpu().numpy()}
-            # savemat('%s.mat'%i, res)
-
-            # out = torch.cat([mx, my, mz], 1).squeeze(1).squeeze(2)  # .detach()
-
-            # time_wise_profile[:,:,i] = out
 
         mx = m[:, 0:m.size()[1]:3]
         my = m[:, 1:m.size()[1]:3]
         mz = m[:, 2:m.size()[1]:3]
-        out = torch.cat([mx, my, mz], 1).squeeze(1).squeeze(2)  # .detach()
+        out = torch.cat([mx, my, mz], 1).squeeze(1).squeeze(2)
         return out
